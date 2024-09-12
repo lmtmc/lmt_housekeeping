4956b37840612f85daa42d98e7209d27f10a4bff
Xia Huang <xia.summer.huang@gmail.com>
2024-09-11 21:54:32 -0400

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+venv_path = '/home/lmtmc_umass_edu/lmt_housekeeping/env'
+sys.path.insert(0, venv_path)
+
+import site
+site.addsitedir(venv_path)
+
+sys.path.insert(0, '/home/lmtmc_umass_edu/lmt_housekeeping')
+
 from app import app
 server = app.server
